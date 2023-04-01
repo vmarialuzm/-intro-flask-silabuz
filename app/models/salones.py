7feff937bbb61c468483041950c01ecb2c30c363
vmarialuzm <vmarialuzm@gmail.com>
2023-04-01 13:14:51 -0500

    feat: CRUD

--- app/models/salones.py
+++ app/models/salones.py
@@ -6,9 +6,9 @@ class Salon(db.Model):
     id = db.Column(db.Integer, autoincrement=True ,primary_key = True)
     aula = db.Column(db.String(20))
     horaEntrada = db.Column(db.Time)
-    alumnos = db.relationship('Alumno', backref='salon', lazy="dynamic")
+    #alumnos = db.relationship('Alumno', backref='salon', lazy="dynamic")
 
     def __init__(self,aula,horaEntrada,alumnos):
         self.aula=aula
         self.horaEntrada=horaEntrada
-        self.alumnos=alumnos
+        #self.alumnos=alumnos
--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
 from app import create_app
 from app.forms import SalonesForm
+from app.db import db
+
+from app.models.salones import Salon
 
 app=create_app()
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    salones_form=SalonesForm()  #instanciamos el objeto              
+    salones_form=SalonesForm()  #instanciamos el objeto 
+
+    if salones_form.validate_on_submit():  
+        salon=Salon(
+            salones_form.aula.data,
+            salones_form.horaEntrada.data,
+        )
+
+        db.session.add(salon)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
     return render_template("salones.html",salones_form=salones_form) #con **user estamos descomprimiendo ese diccionario
+
+@app.route('/salones')
+def salones():
+    salones=Salon.query.all()
+
+    return render_template("lista-salones.html",salones=salones)
 
 @app.route('/ruta-nueva-1')
 def ruta_nueva():
@@ -16,3 +37,11 @@ def ruta_nueva():
 @app.route('/ruta-nueva-1/ruta_html')
 def ruta_nueva_html():
     return render_template("nuevo.html")
+
+#crean automaticamente las migraciones
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
